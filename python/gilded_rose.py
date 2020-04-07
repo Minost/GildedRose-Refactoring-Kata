64df4f3f11c3a41746ef83ba840ed45e4a84dd47
Simon Trembleau <simon.trembleau@dot-squad.io>
2020-04-07 11:12:22 +0200

    Clarify code and add test cases to pass in each conditions

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -20,66 +20,74 @@ class GildedRose(object):
         Update the quality of items contained in the list
         """
         # For each item of items
+        #  - nothing happens for epic items
+        #  - conjured items are considered as standard items for now
         for item in self.items:
             # Condition on item name
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                # Condition on item quality
-                if item.quality > 0:
-                    # Condition on item name
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        # Decrease item quality
-                        item.quality = item.quality - 1
-            else:
+            if item.name != "Sulfuras, Hand of Ragnaros":
+                # Decrease item remaining days for non epic items
+                item.sell_in -= 1
+
+            # Condition on item name
+            if item.name == "Aged Brie" or item.name == "Backstage passes to a TAFKAL80ETC concert":
                 # Condition on item quality
                 if item.quality < 50:
-                    # Increase item quality
-                    item.quality = item.quality + 1
+                    # Increase item quality for aging and ticket items
+                    item.quality += 1
                     # Condition on item name
                     if item.name == "Backstage passes to a TAFKAL80ETC concert":
                         # Condition on item remaining days
                         if item.sell_in < 11:
                             # Condition on item quality
                             if item.quality < 50:
-                                # Increase item quality
-                                item.quality = item.quality + 1
+                                # Increase item quality for ticket items
+                                item.quality += 1
                         # Condition on item remaining days
                         if item.sell_in < 6:
                             # Condition on item quality
                             if item.quality < 50:
-                                # Increase item quality
-                                item.quality = item.quality + 1
-            # Condition on item name
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                # Decrease item remaining days
-                item.sell_in = item.sell_in - 1
+                                # Increase item quality for ticket items
+                                item.quality += 1
+            else:
+                # Condition on item quality
+                if item.quality > 0:
+                    # Condition on item name
+                    if item.name != "Sulfuras, Hand of Ragnaros":
+                        # Decrease item quality for standard items
+                        item.quality -= 1
+
             if item.sell_in < 0:
                 # Condition on item name
-                if item.name != "Aged Brie":
+                if item.name == "Aged Brie":
+                    # Condition on item quality
+                    if item.quality < 50:
+                        # Increase item quality for aging items
+                        item.quality += 1
+                else:
                     # Condition on item name
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                        # Update item quality for passed out ticket items
+                        item.quality = 0
+                    else:
                         # Condition on item quality
                         if item.quality > 0:
                             # Condition on item name
                             if item.name != "Sulfuras, Hand of Ragnaros":
-                                # Decrease item quality
-                                item.quality = item.quality - 1
-                    else:
-                        # Update item quality
-                        item.quality = 0
-                else:
-                    # Condition on item quality
-                    if item.quality < 50:
-                        # Increase item quality
-                        item.quality = item.quality + 1
+                                # Decrease item quality for passed out standard items
+                                item.quality -= 1
 
 
 class Item:
+    """
+    Represent an item that can be sold at the inn.
+    """
+
     def __init__(self, name: str, sell_in: int, quality: int):
         """
         Initialize an item.
         :param name: the name of the item
-        :param name: the number of days to sell the item
-        :param name: the quality of the item
+        :param sell_in: the number of days to sell the item
+        :param quality: the value of the item
         """
         self.name = name
         self.sell_in = sell_in
